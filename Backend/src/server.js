49c1b849f9350c004dae0d170570751ae7d767f4
mturetschek <mturetschek@student.tgm.ac.at>
2023-04-13 13:10:10 +0200

    basic server js structure

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,2 +1,15 @@
 var http = require('http');
 var url = require('url');
+
+http.createServer(function(req, res) {
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, POST, GET, PUT, DELETE, UPDATE",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Max-Age": 2592000,
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true
+        
+    };
+    res.writeHead(200, headers);
+}).listen(8080)

6a471eb385ec7ac7a957acc71de29ebe52ea3752
faust231 <faust@student.tgm.ac.at>
2023-04-13 13:28:31 +0200

    Connect script für sql

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,23 @@
 var http = require('http');
 var url = require('url');
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
 
 http.createServer(function(req, res) {
     const headers = {

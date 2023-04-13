f6e65504ba07fbe12258c0e241fd00bd715ec23f
faust231 <faust@student.tgm.ac.at>
2023-04-13 13:56:26 +0200

    SQL Create Script

--- Backend/src/db/script.sql
+++ Backend/src/db/script.sql
@@ -11,26 +11,21 @@ CREATE TABLE Userdaten (
     Email VARCHAR(255) NOT NULL,
     Passwort VARCHAR(255) NOT NULL,
     Username VARCHAR(255) NOT NULL,
-    UserID INT()
+    UserID INT,
+    PRIMARY KEY (Email)
 );
 
 --
--- Tabelle Futter
+-- Tabelle Spieldaten
 --
 
-CREATE TABLE Futter (
-    Futtersorte VARCHAR(255) NOT NULL,
-    Preis FLOAT(4, 2),
-    PRIMARY KEY (Futtersorte),
-    Anschrift VARCHAR(255)
-);
-
---
--- Tabelle Futterhersteller
---
-
-CREATE TABLE Futterhersteller (
-    Anschrift VARCHAR(255) NOT NULL,
-    Name VARCHAR(255),
-    PRIMARY KEY (Anschrift)
+CREATE TABLE Spieldaten (
+    UserID INT,
+    TTT INT,
+    MS INT,
+    PP INT,
+    Q1 INT,
+    Q2 INT,
+    Q3 INT,
+    PRIMARY KEY (UserID)
 );

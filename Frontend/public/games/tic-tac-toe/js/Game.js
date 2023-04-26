6a0a2261f033b1eb8c4dff328bee73c0aae64020
fhipfinger <fhipfinger@student.tgm.ac.at>
2023-04-26 16:47:43 +0200

    almost done with tictactoe

--- Frontend/public/games/tic-tac-toe/js/Game.js
+++ Frontend/public/games/tic-tac-toe/js/Game.js
@@ -25,7 +25,8 @@ class Game {
             [0,3,6], //a=0, b=3, c=6
             [1,4,7],
             [2,5,8],
-            [2,4,6]
+            [2,4,6],
+            [0,4,8]
         ];
 
         winningCombinations.forEach((wc) => {
